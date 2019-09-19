3acee06a0c42a670871c5aa3fbc06ed2a9470648
Erik Hofer <erik-hofer@mail.de>
2019-09-19 13:38:22 +0200

    wip

--- release.sh
+++ release.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-git remote set origin https://${GITHUB_TOKEN}@github.com/erikhofer/gradle-starter.git #> /dev/null 2>&1
+git remote set-url origin https://${GITHUB_TOKEN}@github.com/erikhofer/gradle-starter.git #> /dev/null 2>&1
 git config --global user.email "erik-hofer@mail.de"
 git config --global user.name "Travis CI"
 git checkout $TRAVIS_BRANCH
