cc7f534fba18d5cc394283bf641962f9bf3047e1
Erik Hofer <erik-hofer@mail.de>
2019-09-19 13:32:22 +0200

    wip

--- release.sh
+++ release.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 
+git remote set origin https://${GITHUB_TOKEN}@github.com/erikhofer/gradle-starter.git > /dev/null 2>&1
+git config --global user.email "travis@travis-ci.org"
+git config --global user.name "Travis CI"
 git checkout $TRAVIS_BRANCH
 ./gradlew release -Prelease.useAutomaticVersion=true -Prelease.releaseVersion=$RELEASE_VERSION
